53f204a30a6594ab665dbb243eef8488d958f9ed
André Cardoso <nuckhouse11@gmail.com>
2022-04-06 01:23:36 +0100

    updated

--- ft_printf.h
+++ ft_printf.h
@@ -1,10 +1,16 @@
-#ifndef FT_PRINTF
+#ifndef FT_PRINTF_H
+
+# define FT_PRINTF_H
 
 # include <stdarg.h>
 # include <stdio.h>
 # include <unistd.h>
 
-void	ft_putchar(char c);
-void    ft_putstr(char *str);
+int		ft_putchar(char c);
+int		ft_putstr(char *str);
+size_t	ft_strlen(const char *s);
+int		ft_putbase(long long num, char *base);
+int		ft_printf(const char *s, ...);
+int		print_case(va_list *args, char ident);
 
 #endif
