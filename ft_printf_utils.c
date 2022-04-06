150a56c4d518e6ade5c86ce0c7574f987524cba9
André Cardoso <nuckhouse11@gmail.com>
2022-04-06 01:22:40 +0100

    ft_putbase working

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -6,27 +6,58 @@
 /*   By: nuck <nuck@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 23:19:46 by nuck              #+#    #+#             */
-/*   Updated: 2022/04/04 23:43:41 by nuck             ###   ########.fr       */
+/*   Updated: 2022/04/06 00:54:11 by nuck             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_putchar(char c)
+size_t	ft_strlen(const char *s)
 {
-	write(1, &c, 1);
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
 }
 
-void	ft_putstr(char *str)
+int	ft_putchar(char c)
 {
+	return (write (1, &c, 1));
+}
+
+int	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
 	while (*str)
 	{
-		write(1, str, 1);
+		i += write(1, str, 1);
 		str++;
 	}
+	return (i);
 }
 
-void	ft_putbase(char *base, long long nbr)
+int	ft_putbase(long long num, char *base)
 {
+	long long	max;
+	int			i;
 
+	i = 0;
+	max = (long long)ft_strlen(base);
+	if (num < 0)
+	{
+		num *= -1;
+		i += write(1, "-", 1);
+	}
+	if (num < max)
+	{
+		i += write(1, &base[num], 1);
+		return (i);
+	}
+	i += ft_putbase(num / max, base);
+	i += write(1, &base[num % max], 1);
+	return (i);
 }
